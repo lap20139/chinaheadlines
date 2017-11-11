4e761d1c1c0899dfba113ec20539d71514006241
lap20139 <910293074@qq.com>
2017-11-11 22:18:52 +0800

    dynamic routing

--- headlines.py
+++ headlines.py
@@ -1,1 +1,36 @@
-import flask from Flask
+# -*- coding: utf-8 -*-
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+from flask  import  Flask
+import feedparser
+
+
+app = Flask(__name__)
+
+RSS_FEEDS = {'china':'http://news.qq.com/newsgn/rss_newsgn.xml',
+             'world':'http://news.qq.com/newsgj/rss_newswj.xml',
+             'keji':'http://news.baidu.com/n?cmd=1&class=technnews&tn=rss',
+             'edu': 'http://news.baidu.com/n?cmd=1&class=edunews&tn=rss'
+             }
+
+@app.route("/")
+@app.route("/<published>")
+def get_news(published="china"):
+    feed=feedparser.parse(RSS_FEEDS[published])
+    first_article = feed['entries'][0]
+    return """<html>
+      <body>
+           <h1>china headlines</h1>
+           <b>{0}</b>
+           <i>{1}</i>
+           <p>{2}</p>
+      </body>
+      </html>""".format(first_article.get("title"),
+                        first_article.get('published'),
+                        first_article.get("summary"))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
