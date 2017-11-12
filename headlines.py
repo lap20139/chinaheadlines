88fd66da31df7dd230bc7b0292e1371bdca6f404
lap20139 <910293074@qq.com>
2017-11-12 20:02:52 +0800

    Using GET

--- headlines.py
+++ headlines.py
@@ -1,25 +1,31 @@
 # encoding: utf-8
 import sys
+
 reload(sys)
 sys.setdefaultencoding('utf8')
 
-from flask  import  Flask
+from flask import Flask
 from flask import render_template
 import feedparser
-
+from flask import request
 
 app = Flask(__name__)
 
-RSS_FEEDS = {'china':'http://news.qq.com/newsgn/rss_newsgn.xml',
-             'world':'http://news.qq.com/newsgj/rss_newswj.xml',
-             'keji':'http://news.baidu.com/n?cmd=1&class=technnews&tn=rss',
+RSS_FEEDS = {'china': 'http://news.qq.com/newsgn/rss_newsgn.xml',
+             'world': 'http://news.qq.com/newsgj/rss_newswj.xml',
+             'keji': 'http://news.baidu.com/n?cmd=1&class=technnews&tn=rss',
              'edu': 'http://news.baidu.com/n?cmd=1&class=edunews&tn=rss'
              }
 
+
 @app.route("/")
-@app.route("/<published>")
-def get_news(published="china"):
-    feed=feedparser.parse(RSS_FEEDS[published])
+def get_news():
+    query = request.args.get('publication')
+    if not query or query.lower() not in RSS_FEEDS:
+        publication = "china"
+    else:
+        publication = query.lower()
+    feed = feedparser.parse(RSS_FEEDS[publication])
     # first_article = feed['entries'][0]
     return render_template("home.html", articles=feed['entries'])
 
